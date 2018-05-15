0a66599d77df0c3daaaef9b155199033b00e5738
Rod McNew <rmcnew@relivinc.com>
2018-05-15 12:33:59 -0500

    upgrade from ga to gtag

--- src/Api/Analytics/GetCurrentAnalyticEntity.php
+++ src/Api/Analytics/GetCurrentAnalyticEntity.php
@@ -10,36 +10,52 @@ use Reliv\RcmGoogleAnalytics\Api\Site\GetCurrentSiteId;
  */
 class GetCurrentAnalyticEntity
 {
+    const TRACKING_ID_OVERRIDE_KEY = 'tracking_id_override';
+
     protected $getCurrentSiteId;
     protected $getAnalyticEntityForSite;
+    protected $config;
 
     /**
-     * @param GetCurrentSiteId         $getCurrentSiteId
+     * @param GetCurrentSiteId $getCurrentSiteId
      * @param GetAnalyticEntityForSite $getAnalyticEntityForSite
      */
     public function __construct(
         GetCurrentSiteId $getCurrentSiteId,
-        GetAnalyticEntityForSite $getAnalyticEntityForSite
+        GetAnalyticEntityForSite $getAnalyticEntityForSite,
+        $config
     ) {
         $this->getCurrentSiteId = $getCurrentSiteId;
         $this->getAnalyticEntityForSite = $getAnalyticEntityForSite;
+        $this->config = $config['Reliv\RcmGoogleAnalytics'];
     }
 
     /**
      * @param ServerRequestInterface $request
-     * @param null                   $default
+     * @param null $default
      *
      * @return null|\Reliv\RcmGoogleAnalytics\Entity\RcmGoogleAnalytics
      */
-    public function __invoke(
+    public
+    function __invoke(
         ServerRequestInterface $request,
         $default = null
     ) {
         $siteId = $this->getCurrentSiteId->__invoke($request);
 
-        return $this->getAnalyticEntityForSite->__invoke(
+        $entity = $this->getAnalyticEntityForSite->__invoke(
             $siteId,
             $default
         );
+
+        if (!empty($this->config[self::TRACKING_ID_OVERRIDE_KEY])) {
+            /**
+             * Allow the ID to be overriden in config. Is useful for development environments
+             * to keep events from going to the real account id.
+             */
+            $entity->setTrackingId($this->config[self::TRACKING_ID_OVERRIDE_KEY]);
+        }
+
+        return $entity;
     }
 }
